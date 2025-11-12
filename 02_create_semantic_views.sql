76c58916257840f350cab7837699af98ff25b44f
Bharath Suresh <bharathsuresh.86@gmail.com>
2025-11-12 17:31:37 +0530

    Fix: Remove calibration_technician dimension that causes nested JSON path extraction error

--- 02_create_semantic_views.sql
+++ 02_create_semantic_views.sql
@@ -152,7 +152,6 @@ DIMENSIONS (
   iot.primary_sensor_name AS iot.sensor_readings[0]:sensor_name::STRING,
   iot.primary_sensor_status AS iot.sensor_readings[0]:status::STRING,
   iot.machine_state AS iot.machine_state:state::STRING,
-  iot.calibration_technician AS iot.calibration_data:calibration_technician::STRING,
   iot.overall_status AS CASE 
     WHEN ARRAY_SIZE(iot.machine_state:alerts) > 0 THEN 'Alert'
     WHEN ARRAY_SIZE(iot.machine_state:warnings) > 0 THEN 'Warning'
